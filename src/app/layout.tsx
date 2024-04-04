a9d96819e3511aa29fd00363bdacc8dcc91df667
b7r6 <b7r6@b7r6.net>
2024-04-04 23:08:55 +0700

    kthx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,6 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 
-// import { Aldrich } from "next/font/google";
-
-// const variableFont = Aldrich({ subsets: ["latin"], weight: "400" });
-
 export const metadata: Metadata = {
   title: "kthx",
   description: "xz.team",
